dfaaae220d9efaa8fc5a521b135d2bd1b7663c11
redsemin <semin1125@gmail.com>
2025-10-26 20:08:21 +0900

    semin

---
+++ MotionService/src/main/java/kopo/motionservice/dto/LastPhraseDTO.java
@@ -0,0 +1,12 @@
+package kopo.motionservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LastPhraseDTO {
+    private String phrase;
+}
